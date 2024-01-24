72482f928ed4f2dd71b48d29db33d8b04dd0b7af
Daniel Rodrigues <rcdan22@gmail.com>
2024-01-24 01:43:41 +0000

    added code to resize all images in a directory

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fyp_comics
+A repository to hold the python notebooks and any additional code used during my BSc final year project.
--- page-resizer/page_resizer.py
+++ page-resizer/page_resizer.py
@@ -1,13 +1,55 @@
+from PIL import Image
+import os
 
-def is_square():
-    pass
 
-def calculate_gutters():
-    pass
+def is_square(img):
+    return img.size[0] == img.size[1]
 
-def change_to_square():
-    pass
 
-def resize_square_image(target_size = 512):
-    pass
+def calculate_gutters(img):
+    return round((max(img.size) - min(img.size)) / 2)
 
+
+def do_padding(img, padding, pad_x):
+    w = max(img.size)
+    h = w  # since we're making the img square
+    res = Image.new(img.mode, (w, h), (255, 255, 255))
+    left, top = 0, 0
+    if pad_x:
+        top = padding
+    else:
+        left = padding
+    res.paste(img, (left, top))
+    return res
+
+
+def change_to_square(img):
+    pad_x = img.size[0] > img.size[1]  # if not pad_x, then pad y. Increase width along that specific axis.
+    padding = calculate_gutters(img)
+    return do_padding(img, padding, pad_x)
+
+
+def resize_square_image(img, target_size = 512):
+    return img.resize((target_size, target_size))
+
+
+def resize_image(filepath):
+    img = Image.open(filepath)
+    final = img.copy
+    if not is_square(img):
+        final = change_to_square(img)
+    return resize_square_image(final)
+
+
+def resize_all():
+    input_path = 'input-images/'
+    imgs = os.listdir(input_path)
+    for img in imgs:
+        res = resize_image(input_path + img)
+        res.save('output-images/' + img)
+        print('Completed image:', img)
+    print('COMPLETED ALL!')
+
+
+if __name__ == '__main__':
+    resize_all()
